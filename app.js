98c3c344cd7c7412a4827c95464a604a9a64e53a
edsonbastos2 <edsonbastos2@hotmail.com>
2019-10-16 22:43:12 -0300

    consumindo API
    Pegando imagens de uma api.

--- app.js
+++ app.js
@@ -38,18 +38,28 @@ function show(){
 }
 
 /*Consumir API rest*/
-const url = 'https://dt11.pepblast.com/pzAlbums_php03/PSO/PSOApi.php?client=HTML&app=PLUGIN&m=pso.remix.getRemixPars&args={"rid":"x85in000149b85wn0lamsj","av":"v1","esort":0,"easc":0,"getappelements":1,"getinputusers":1,"getpropsjs":1,"getisfollower":1,"usecreatoresort":1,"enmax":120}';
-//const url = 'https://jsonplaceholder.typicode.com/users'
-fetch(url)
-    .then( (response) => response.json())
-    .then(function(data){ 
-        console.log(data)
+const api = fetch('https://dt11.pepblast.com/pzAlbums_php03/PSO/PSOApi.php?client=HTML&app=PLUGIN&m=pso.remix.getRemixPars&args={%22rid%22:%22x85in000149b85wn0lamsj%22,%22av%22:%22v1%22,%22esort%22:0,%22easc%22:0,%22getappelements%22:1,%22getinputusers%22:1,%22getpropsjs%22:1,%22getisfollower%22:1,%22usecreatoresort%22:1,%22enmax%22:120}');
+
+const urlImg = 'https://movt11.pepblast.com/RMX_PHOTO/'
+
+const extensaoImg = '.jpg';
+
+api.then(r => r.json())
+
+    .then(data => {
+
+        let info = data.getRemixApprovedElements.its_media;
+
+        let arrayImg = [];
+
+        info.forEach(function (img) {
+            image = urlImg + img.eid + extensaoImg;
+            let elemento = document.getElementById('imageBg');
+            let div = document.createElement('div');
+            div.innerHTML = `<img src=${image} />`;
+            elemento.appendChild(div);
+            console.log(image);
+
+        });
         
-        return data.map(function(obj){
-            let li = document.createElement('li');
-            li.innerHTML = `${obj} ${obj}`
-            ul.appendChild(li)
-        })
-    })
-    .catch(err => console.log(err))
->>>>>>> ab07b283597d586f3e175abeaa364c60c12a1a93
+    });
